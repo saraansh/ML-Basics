907ec93f6adc9e24aac8e91896a8fcef1597f024
saraansh <bsaraansh@gmail.com>
2017-07-29 10:07:06 +0530

    Updated Data Preprocessing Template

--- Python/Part 1 - Data Preprocessing/data_preprocessing.py
+++ Python/Part 1 - Data Preprocessing/data_preprocessing.py
@@ -16,7 +16,7 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
 
-"""# Fill up missing values with Mean
+# Fill up missing values with Mean
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(X[:,1:3])
@@ -32,14 +32,14 @@ onehotencode_x = OneHotEncoder(categorical_features=[0])
 X = onehotencode_x.fit_transform(X).toarray()
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)"""
+y = labelencoder_y.fit_transform(y)
 
 # Splitting dataset into Training Set and Test Set
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-"""# Feature Scaling
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)"""
+x_test = sc_x.transform(x_test)
