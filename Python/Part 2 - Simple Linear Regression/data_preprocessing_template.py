8d394f6436b7a39b1d6907cdac87077ee62ed908
saraansh <bsaraansh@gmail.com>
2017-07-30 18:47:06 +0530

    Modified Data Proceessing Template

--- Python/Part 2 - Simple Linear Regression/data_preprocessing_template.py
+++ Python/Part 2 - Simple Linear Regression/data_preprocessing_template.py
@@ -8,7 +8,7 @@ Created on Sat Jul 29 10:10:36 2017
 
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing dataset from CSV file
@@ -20,8 +20,8 @@ y = dataset.iloc[:,3].values
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-"""# Feature Scaling
-from sklearn.preprocessing import StandardScaler
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)"""
