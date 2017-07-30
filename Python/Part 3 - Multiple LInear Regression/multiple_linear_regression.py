5ee4ec1d889be5dc5f115fde57c992844a861197
saraansh <bsaraansh@gmail.com>
2017-07-30 20:48:20 +0530

    Multiple Linear Regression Template

---
+++ Python/Part 3 - Multiple LInear Regression/multiple_linear_regression.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 30 20:10:28 2017
+
+@author: cyanide
+"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing dataset from CSV file
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,4].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable (Not-needed)
+"""labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)"""
+
+# Avoiding the Dummy Variable Trap
+# Python Libraries take care of it by default
+# so no need to add it in the future
+X = X[:, 1:]
+
+# Splitting dataset into Training Set and Test Set
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)"""
