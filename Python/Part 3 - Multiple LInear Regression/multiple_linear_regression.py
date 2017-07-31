3f9e1cb492b9ef714f5806f08215907bc933eb7a
saraansh <bsaraansh@gmail.com>
2017-07-31 19:56:42 +0530

    Multiple Linear Regression

--- Python/Part 3 - Multiple LInear Regression/multiple_linear_regression.py
+++ Python/Part 3 - Multiple LInear Regression/multiple_linear_regression.py
@@ -40,3 +40,11 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)"""
+
+# Fitting Multiple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(x_test)
