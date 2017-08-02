3c07120a1e3b51a4548c5b5a40d0a7a1704029d7
saraansh <bsaraansh@gmail.com>
2017-08-03 00:51:32 +0530

    Minor Changes

--- Matlab-Octave/Example 2/costFunction.m
+++ Matlab-Octave/Example 2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,13 +20,14 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+hypo = sigmoid(X * theta); %hypothesis
+calc = -(y .* log(hypo) + (1-y) .* log(1-hypo));
+J = (1/m) * sum(calc);
 
-
-
-
-
-
-
+for i = 1:m,
+    grad = grad + ((hypo(i) - y(i)) * X(i, :)');
+end
+grad = (1/m) * grad;
 % =============================================================
 
 end
